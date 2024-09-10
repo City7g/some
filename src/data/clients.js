f047ddccf22c9711967ae3209ac04e834523bffe
HunchakAndrii <andreygunchak1@gmail.com>
2024-09-10 12:24:25 +0300

    clients section desktop version

---
+++ src/data/clients.js
@@ -0,0 +1,34 @@
+export const clientsData = [
+  {
+    src: 'img/clients/client-item-1.png',
+    fullName: 'Mira Carder',
+    workplace: 'Hey Inc',
+    job: 'Marketing Director',
+    country: 'USA',
+    countryFlag: 'img/flags/usa.svg',
+  },
+  {
+    src: 'img/clients/client-item-2.png',
+    fullName: 'Gustavo Korsgaard',
+    workplace: 'Sia Partner',
+    job: 'CEO',
+    country: 'USA',
+    countryFlag: 'img/flags/usa.svg',
+  },
+  {
+    src: 'img/clients/client-item-3.png',
+    fullName: 'Tatiana Bator',
+    workplace: 'Sia Partner',
+    job: 'CEO',
+    country: 'USA',
+    countryFlag: 'img/flags/usa.svg',
+  },
+  {
+    src: 'img/clients/client-item-4.png',
+    fullName: 'Carla Levin',
+    workplace: 'Sia Partner',
+    job: 'CEO',
+    country: 'USA',
+    countryFlag: 'img/flags/usa.svg',
+  },
+]
