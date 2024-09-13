9e3395b400f72650cc3570f63882932eced0f93b
HunchakAndrii <andreygunchak1@gmail.com>
2024-09-13 14:16:01 +0300

    rename location adn fixed flag

--- src/data/vacation-database.js
+++ src/data/vacation-database.js
@@ -41,37 +41,3 @@ export const vacanciesData = {
     },
   ],
 }
-  // {
-  //   date: 'June 25, 2024',
-  //   countryFlag: 'img/flags/usa.svg',
-  //   location: 'New York, USA',
-  //   job: 'Senior Market Analyst',
-  //   description:
-  //     'We are looking for a Senior Market Analyst to join our team and contribute to our company’s growth by providing in-depth analysis of market trends, competitive landscapes, and consumer preferences. The ideal candidate will be responsible for developing strategies that help our clients achieve their business goals.',
-    
-  // },
-  // {
-  //   date: 'June 25, 2024',
-  //   countryFlag: 'img/flags/usa.svg',
-  //   location: 'New York, USA',
-  //   job: 'Senior Market Analyst',
-  //   description:
-  //     'We are looking for a Senior Market Analyst to join our team and contribute to our company’s growth by providing in-depth analysis of market trends, competitive landscapes, and consumer preferences. The ideal candidate will be responsible for developing strategies that help our clients achieve their business goals.',
-  // },
-  // {
-  //   date: 'June 25, 2024',
-  //   countryFlag: 'img/flags/usa.svg',
-  //   location: 'New York, USA',
-  //   job: 'Senior Market Analyst',
-  //   description:
-  //     'We are looking for a Senior Market Analyst to join our team and contribute to our company’s growth by providing in-depth analysis of market trends, competitive landscapes, and consumer preferences. The ideal candidate will be responsible for developing strategies that help our clients achieve their business goals.',
-  // },
-  // {
-  //   date: 'June 25, 2024',
-  //   countryFlag: 'img/flags/usa.svg',
-  //   location: 'New York, USA',
-  //   job: 'Senior Market Analyst',
-  //   description:
-  //     'We are looking for a Senior Market Analyst to join our team and contribute to our company’s growth by providing in-depth analysis of market trends, competitive landscapes, and consumer preferences. The ideal candidate will be responsible for developing strategies that help our clients achieve their business goals.',
-  // },
-// ]
